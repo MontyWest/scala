51a5b637ddf37031fa633c0bd607eef069cec6e0
MontyWest <monty.p.west@gmail.com>
2015-04-22 16:06:53 +0100

    coder tests

--- src/test/playfair/CoderSpec.scala
+++ src/test/playfair/CoderSpec.scala
@@ -12,7 +12,7 @@ class CoderSpec extends FlatSpec{
   }
   
   "decode" should "apply cipher square to ciphertext to produce (partial) plaintext" in {
-    //TODO add Mocking
+    //TODO add Mocking on TextPairs and Cipher
     val coder: Coder =  Coder("PennsylvaniaJ")
     
     val ct: String = "gsvqrenkisynisynigqwknsxzt"
@@ -20,6 +20,16 @@ class CoderSpec extends FlatSpec{
     
     assert(expected == coder.decode(ct))
     
+  }
+  
+  "encode" should "apply cipher square to plaintext and produce ciphertext" in {
+    //TODO add Mocking on TextPairs and Cipher
+    val coder: Coder =  Coder("PennsylvaniaJ")
+    
+    val pt: String = "Jimmy possesses JOOP! ~xx~"
+    val expected: String = "gsvqrenkisynisynigqwknsxzt"
+
+    assert(expected == coder.encode(pt))
   }
   
 }
